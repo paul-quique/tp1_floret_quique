084e3e17799e1ca5b6385df831bd059612608898
Paul QUIQUE <paulquique@protonmail.com>
2021-01-29 11:43:42 +0100

    premiere fonction prliminaire

--- gestion_promo.py
+++ gestion_promo.py
@@ -1,3 +1,25 @@
 from etudiants import L_ETUDIANTS
 
-print(L_ETUDIANTS)
+#Travail préliminaire
+
+def pour_tous(liste):
+    """
+    Paramètres: liste une liste de booléens
+    Valeur de retour: un booléen (True|False)
+    Comportement: Renvoie False si la liste
+    contient un élément False, True sinon.
+    >>> pour_tous([])
+    True
+    >>> pour_tous((True, True, True))
+    True
+    >>> pour_tous((True, False, True))
+    False
+    """
+    if len(liste) == 0:
+        return True
+    for i in liste:
+        if not(i):
+            return False
+    return True
+
+
